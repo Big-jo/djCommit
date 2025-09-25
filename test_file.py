719a33dc4ce17eac86a79fb3516202f5142d66a6
joseph <furiousjoe16@gmail.com>
2025-09-25 22:21:27 +0100

    Add test function for Git Hook DJ

---
+++ test_file.py
@@ -0,0 +1,7 @@
+def hello_world():
+    """A simple function to test the Git Hook DJ."""
+    print("Hello, World!")
+    return "success"
+
+# This is a test file for the Git Hook DJ
+# It should trigger a "stellar" classification because it adds a new function
